fed74f1cbfefee3c55b0a88d7b5e82aba338e614
SantiSanGal <santiagon.g.m@gmail.com>
2023-08-13 16:36:52 -0400

    chat grupal terminado

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,17 @@
 const socket = io();
+
+const colors = ["#fc9671", "#ffd26c", "#53a6fd", "#4858ff", "#f15c5a", "#23a03b", "#AA00FF"]
+
+const randomColor = () => {
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    return color
+};
+
 let data = {
     usuario: '',
-    pass: ''
+    pass: '',
 }
+
 let lastUser = ''
 
 $(document).ready(()=>{
@@ -29,10 +38,14 @@ socket.on("showMessage", data => {
     if (lastUser == data.usuario) {
         message += "<p class=\"chat-messages-message-text\">" + data.message + "</p>"
     }else{
-        message += "<strong class=\"chat-messages-message-user\">" + data.usuario + " </strong>"
+        message += "<strong id=\"chat-messages-message-user-"+ data.usuario +"\" class=\"chat-messages-message-user chat-messages-message-user-"+ data.usuario +"\">" + data.usuario + " </strong>"
         message += "<p class=\"chat-messages-message-text\">" + data.message + "</p>"
     }
     message += "</span>"
     lastUser = data.usuario
     $("#chat-messages").append(message);
+    console.log(data.color, typeof(data.color));
+    $(".chat-messages-message-user-"+ data.usuario).css("color", randomColor());
 });
+
+
