01dc71e246d3c802f67bfe450dcd25833fad73d0
SantiSanGal <santiagon.g.m@gmail.com>
2023-08-16 22:17:16 -0400

    guardando usuarios y ids

--- sockets/server.js
+++ sockets/server.js
@@ -4,8 +4,6 @@
 //!cambiar en scripts del package.json nodemon sockets/server.js
 // * npm start
 
-let usuariosConectados = [];
-
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
@@ -17,19 +15,27 @@ app.use(express.static('public'))
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
+let userOnId =  new Array();
+let idsOnUser = new Array();
+
 io.on("connection", socket => {
     console.log("Nueva conexión, ", socket.id);
     
     socket.on("loginForm", data => {
-        usuariosConectados.push({usuario: data.usuario, pass: data.pass, id: socket.id})    
-        console.log(usuariosConectados);
+        userOnId[socket.id] = data.usuario;
+        if (idsOnUser[data.usuario]) {
+            idsOnUser[data.usuario] = new Array();
+        }else{
+            idsOnUser[data.usuario].push(socket.id);
+        }
     });
 
     socket.on("sendMessage", data => {
         console.log('Message: ', data);
         io.to(data.idUser).emit("showMessage", data);
-    });
-
+    });    
+    
 });
+
 
 httpServer.listen(3000);
