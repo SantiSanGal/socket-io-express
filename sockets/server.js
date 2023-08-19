bb3865e7a5304d8c8f6f2d8ca144275c93b4b6b6
SantiSanGal <santiagon.g.m@gmail.com>
2023-08-18 23:58:33 -0400

    comentarios

--- sockets/server.js
+++ sockets/server.js
@@ -15,16 +15,22 @@ app.use(express.static('public'))
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
+//arrayObject para usuarios conectados
 let usuariosConectados = [];
+
+//array de arrays para ids de usuarios con conexiones multiples
 let idsUsuarios = [];
 
 io.on("connection", socket => {
     console.log("Nueva conexión, ", socket.id);
     
     socket.on("loginForm", data => {
+        //juntamos todos los usuarios conectados
         usuariosConectados.push({usuario: data.usuario, id: socket.id});
+        //si no existe el usuario, crea un espacio
         if(!idsUsuarios[data.usuario]){
             idsUsuarios[data.usuario] = [socket.id];
+        //si ya existe, pushea el id
         }else{
             idsUsuarios[data.usuario].push(socket.id);
         }
@@ -36,12 +42,19 @@ io.on("connection", socket => {
     });
 
     socket.on("disconnect", () =>{
+        //verifico si hay algún usuario loggeado
         if (usuariosConectados.length > 0) {
+            //obtengo el usuario que se desconectó 
             [usuarioDesconectado] = usuariosConectados.filter(conectados => conectados.id == socket.id)
+            //de los usuarios conectados, saco el que se desconectó ahora
+            // ! TODO verificar si el usuario tiene una o más conexiones antes de borrar
             usuariosConectados = usuariosConectados.filter(conectados => conectados.id != socket.id)
+            //elimino el id desconectado
             idsUsuarios[usuarioDesconectado.usuario].map((curr, i) => {
                 curr == usuarioDesconectado.id ? idsUsuarios[usuarioDesconectado.usuario].splice(i, 1) : '';
             });
+            //si ya no hay ids conectados, borro el usuario del array
+            // ? verificar si puedo hacer acá lo de TODO 44
             idsUsuarios[usuarioDesconectado.usuario].length < 1 ? delete idsUsuarios[usuarioDesconectado.usuario] : '';
         }
     });
